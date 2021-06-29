d74d32f199f71d355598db6b74c3000070c38cbd
Maddlyn Sivilli <mcsivilli@gmail.com>
2021-06-29 09:38:26 -0400

    Initial Commit

---
+++ MaddlynGitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaddlynGitHubPractice
+//
+//  Created by Maddy Sivilli on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
